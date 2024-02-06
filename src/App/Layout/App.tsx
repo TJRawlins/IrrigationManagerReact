eaadf42009d54e0c6fe4d579d91919b8544c8d8a
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-02-06 11:20:13 -0700

    minor changes app.tsx

--- src/App/Layout/App.tsx
+++ src/App/Layout/App.tsx
@@ -1,9 +1,9 @@
 import Navbar from "./Navbar";
 import "/src/App.css";
-import { Container, CssBaseline, Grid, ThemeProvider } from "@mui/material";
+import { CssBaseline, Grid, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import RouterComponent from "../../routes/RouterComponent";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 // import ZonesMain from "../../Components/zones/ZonesMain";
 // import PlantMain from "../../Components/plants/PlantMain";
 
@@ -21,31 +21,28 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <BrowserRouter>
-        <Grid
-          className="nav-grid"
-          container
-          direction={"column"}
-          flexShrink={0}
-          zIndex={"999"}
-          sx={{ position: "fixed" }}
-        >
-          <Grid className="navbar">
-            <Navbar />
+        <Router>
+          <Grid
+            className="nav-grid"
+            container
+            direction={"column"}
+            flexShrink={0}
+            zIndex={"999"}
+            sx={{ position: "fixed" }}
+          >
+            <Grid className="navbar">
+              <Navbar />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid
-          className="content-grid"
-          container
-          flexGrow={0}
-          sx={{ bgcolor: "#fff", padding: "30px" }}
-        >
-          <RouterComponent />
-          {/* <ZonesMain /> */}
-          {/* <PlantMain /> */}
-        </Grid>
-        </BrowserRouter>
-        <Container maxWidth={false} disableGutters></Container>
+          <Grid
+            className="content-grid"
+            container
+            flexGrow={0}
+            sx={{ bgcolor: "#fff", padding: "30px" }}
+          >
+            <RouterComponent />
+          </Grid>
+        </Router>
       </ThemeProvider>
     </>
   );
