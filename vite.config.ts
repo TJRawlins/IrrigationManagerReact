18c54a4f536d1c76f00ee2aaa493fe551dcc1530
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-02 21:24:25 -0700

    Added proxy server for external Trefle API

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    https: {
-      key: "./ssl/localhost.key",
-      cert: "./ssl/localhost.crt",
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env.VITE_TREFLE_API_KEY;
+  process.env.VITE_API_PORT;
+  process.env.VITE_PORT;
+  return defineConfig({
+    server: {
+      https: {
+        key: "./ssl/localhost.key",
+        cert: "./ssl/localhost.crt",
+      },
+      port: parseInt(process.env.VITE_PORT)
     },
-    port: 3000,
-  },
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
-});
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+    },
+  });
+};
