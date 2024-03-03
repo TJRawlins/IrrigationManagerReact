32ac27c907c5ecefba3d4a0b29ceac78729636aa
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-03 12:15:19 -0700

    Updated header for origin to https

--- proxy/server.ts
+++ proxy/server.ts
@@ -14,7 +14,7 @@ const app = express();
 
 app.get(`/trefle/api/`, (req, res) => {
   // Set headers for cors to allow requests from the react app
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
