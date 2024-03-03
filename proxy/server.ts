92db153108662212ced801c464af0764707e556f
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-02 21:24:25 -0700

    Added proxy server for external Trefle API

---
+++ proxy/server.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+const PORT = process.env.VITE_API_PORT || 5000;
+const app = express();
+
+app.get("/trefle/api", (req, res) => {
+  console.log(req);
+  axios
+    .get(
+      `https://trefle.io/api/v1/plants/search?token=${process.env.VITE_TREFLE_API_KEY}&q=peach`
+    )
+    .then((response) => res.json(response.data));
+});
+
+// app.get("/api", (req, res) => {
+//     console.log(req)
+//     res.json({success: true})
+// });
+
+// Enable Cors
+app.use(cors());
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}...`));
