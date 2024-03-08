b48cd9f44cdfe845a5e2f4d41f203067743c8c60
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-08 01:11:21 -0700

    Disabled no-debugger to allow debugging

--- src/Components/plants/PlantList.tsx
+++ src/Components/plants/PlantList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-debugger */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Button, ButtonGroup, Popover, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
