d30be60cca5ff7bdb199f1afba4f7152e6757b5c
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-09 21:44:35 -0700

    Added default values to plant and zone models

--- src/App/Models/Plant.ts
+++ src/App/Models/Plant.ts
@@ -1,11 +1,11 @@
 export class Plant {
-  id!: number;
-  type!: string;
-  name!: string;
-  galsPerWk!: number;
-  quantity!: number;
-  emittersPerPlant!: number;
-  emitterGPH!: number;
-  timeStamp!: Date | null;
-  zoneId!: number;
+  id: number = 0;
+  type: string = "";
+  name: string = "";
+  galsPerWk: number = 0;
+  quantity: number = 0;
+  emittersPerPlant: number = 0;
+  emitterGPH: number = 0;
+  timeStamp: Date | null = null;
+  zoneId: number = 0;
 }
