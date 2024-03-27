43c07cc62a54662f55586eacd7539cfbf972628d
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-03-26 23:54:14 -0700

    Added box-shadow to zone cards

--- src/Components/zones/ZoneCard.tsx
+++ src/Components/zones/ZoneCard.tsx
@@ -163,7 +163,7 @@ export default function ZoneCard({
         onMouseLeave={handelMouseLeave}
         sx={{
           position: "relative",
-          boxShadow: "none !important",
+          boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px !important",
           borderRadius: "15px",
           padding: "10px",
           width: "300px",
