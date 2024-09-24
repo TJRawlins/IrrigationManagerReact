664f88103156eede3870023a0088c9f44a187040
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-09-23 22:39:50 -0700

    Update Plant HarvestMonth property type from int to string

--- src/App/Models/Plant.ts
+++ src/App/Models/Plant.ts
@@ -7,5 +7,11 @@ export class Plant {
   emittersPerPlant: number = 0;
   emitterGPH: number = 0;
   timeStamp: Date | null = null;
+  imagePath: string = "";
+  age: number = 0;
+  hardinessZone: number = 1;
+  harvestMonth: string = "";
+  exposure: string = "";
+  notes: string = "";
   zoneId: number = 0;
 }
