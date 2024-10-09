643d349395b843c9c6940d198479da2064f61b72
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-10-09 01:02:41 -0700

    Added error boundary component

---
+++ src/Components/errorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  fallback: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  state = { hasError: false };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static getDerivedStateFromError(_error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
