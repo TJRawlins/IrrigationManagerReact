218e83f3b15aa59951b54cc592958c428d6604f2
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-10-13 22:49:15 -0700

    Added firebase directory and file with config

---
+++ src/App/firebase/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "***REMOVED***",
+  authDomain: "***REMOVED***",
+  projectId: "***REMOVED***",
+  storageBucket: "***REMOVED***.appspot.com",
+  messagingSenderId: "***REMOVED***",
+  appId: "1:***REMOVED***:web:ee0a6bf255a85ddaab58d8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
