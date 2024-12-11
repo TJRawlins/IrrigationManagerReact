2f6d67d2b912630e532a1b82385091a900b355df
Tony Rawlins <tjrawlins.dev@gmail.com>
2024-12-10 23:37:36 -0700

    updated zone and plant page styling

--- src/pages/ZonePage/ZonesPage.tsx
+++ src/pages/ZonePage/ZonesPage.tsx
@@ -37,7 +37,7 @@ const ZonesPage = () => {
   const zonePageColorTheme = () => {
     return {
       zoneGrid: {
-        backgroundColor: colors.whiteBlue.varyDark,
+        backgroundColor: colors.whiteBlue.toDarkGray,
       },
     };
   };
