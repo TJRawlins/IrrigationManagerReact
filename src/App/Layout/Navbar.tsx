138d044a87fd71615b925fa07a0c3d4d7bd7bfac
Tony Rawlins <tjrawlins.dev@gmail.com>
2025-05-29 23:19:16 -0700

    update navbar

--- src/App/Layout/Navbar.tsx
+++ src/App/Layout/Navbar.tsx
@@ -29,59 +29,8 @@ import { useNavBarColorTheme } from "../../theme/NavBarTheme";
 
 export default function Navbar() {
   const theme = useTheme();
-  // const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const navBarColorTheme = useNavBarColorTheme();
-  // const appBarStyles = (theme: Theme) => {
-  //   return {
-  //     mainBar: {
-  //       backgroundColor: colors.tertiary.vary,
-  //       color: colors.white.altPrimary,
-  //       zIndex: theme.zIndex.drawer + 1,
-  //       position: "relative !important",
-  //     },
-  //   };
-  // };
-
-  // const Search = styled("div")(({ theme }) => ({
-  //   position: "relative",
-  //   borderRadius: theme.shape.borderRadius,
-  //   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  //   "&:hover": {
-  //     backgroundColor: alpha(theme.palette.common.white, 0.25),
-  //   },
-  //   marginRight: theme.spacing(2),
-  //   marginLeft: 0,
-  //   width: "100%",
-  //   [theme.breakpoints.up("sm")]: {
-  //     marginLeft: theme.spacing(3),
-  //     width: "auto",
-  //   },
-  // }));
-
-  // const SearchIconWrapper = styled("div")(({ theme }) => ({
-  //   padding: theme.spacing(0, 2),
-  //   height: "100%",
-  //   position: "absolute",
-  //   pointerEvents: "none",
-  //   display: "flex",
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // }));
-
-  // const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  //   color: "inherit",
-  //   "& .MuiInputBase-input": {
-  //     padding: theme.spacing(1, 1, 1, 0),
-  //     // vertical padding + font size from searchIcon
-  //     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-  //     transition: theme.transitions.create("width"),
-  //     width: "100%",
-  //     [theme.breakpoints.up("md")]: {
-  //       width: "20ch",
-  //     },
-  //   },
-  // }));
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
