d6d323fcd7bc72e0e86d6d37e825c0594ff6a71f
Tony Rawlins <tjrawlins.dev@gmail.com>
2025-07-08 23:10:27 -0700

    Removed unused functions and interface, streamlining the code for better maintainability for Navbar.

--- src/App/Layout/Navbar.tsx
+++ src/App/Layout/Navbar.tsx
@@ -1,9 +1,7 @@
 import { useTheme } from "@mui/material/styles";
-import Toolbar from "@mui/material/Toolbar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { MdOutlineBrandingWatermark as ZonesIcon } from "react-icons/md";
@@ -32,14 +30,6 @@ export default function Navbar() {
   const colorMode = useContext(ColorModeContext);
   const { open, setOpen } = useDrawer();
   const { sidePanel } = useAppTheme();
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   // Navigation menu items for the top section
   const topMenuItems = [
@@ -198,7 +188,6 @@ export default function Navbar() {
 
 // ===================== STYLED COMPONENTS =====================
 import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Typography from "@mui/material/Typography";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -237,34 +226,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
 }));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => {
-  const { navBar } = useAppTheme();
-  return {
-    zIndex: theme.zIndex.drawer + 1,
-    background: navBar.mainBar.backgroundColor,
-    color: navBar.mainBar.color,
-    borderBottom: navBar.mainBar.borderBottom,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(["width", "margin"], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  };
-});
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -320,10 +281,6 @@ const StyledLink = styled(Link)`
 const LogoContainer = styled(Box)`
   display: flex;
   align-items: center;
-`;
-
-const Spacer = styled(Box)`
-  flex-grow: 1;
 `;
 
 const StyledListItem = styled(ListItem)`
