89c672ac6976ff581e9699942214a154a7d3de22
Tony Rawlins <tjrawlins.dev@gmail.com>
2025-07-22 20:07:24 -0700

    Refactor ZoneCardHeader component: improve code readability by restructuring imports and formatting, enhance subheader styling, and streamline action handling for better maintainability.

--- src/Components/zones/zoneCard/ZoneCardHeader.tsx
+++ src/Components/zones/zoneCard/ZoneCardHeader.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { CardHeader, IconButton, Menu, MenuItem, Popover, Button } from "@mui/material";
+import {
+  CardHeader,
+  IconButton,
+  Menu,
+  MenuItem,
+  Popover,
+  Button,
+} from "@mui/material";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { BsCalendar4Week } from "react-icons/bs";
 import { RiTimerLine } from "react-icons/ri";
@@ -47,141 +54,153 @@ const ZoneCardHeader: React.FC<ZoneCardHeaderProps> = ({
   deleteZone,
 }) => {
   const { zoneCard, messages, fonts } = useAppTheme();
-  
-    return (
+
+  return (
     <StyledCardHeader
       sx={{ color: zoneCard.text.color }}
       title={
-        <ZoneTitle style={{ ...fonts.headers}}>
-          {zone.name.length > 18 ? zone.name.substring(0, 18) + "..." : zone.name}
+        <ZoneTitle style={{ ...fonts.headers }}>
+          {zone.name.length > 18
+            ? zone.name.substring(0, 18) + "..."
+            : zone.name}
         </ZoneTitle>
       }
       subheader={
         <CardSubheader>
           <CardSubheaderItem>
-            <RiTimerLine /> {zone.runtimeHours}h {zone.runtimeMinutes}m
+            <RiTimerLine />
+            {zone.runtimeHours}h {zone.runtimeMinutes}m
           </CardSubheaderItem>
           <CardSubheaderItem>
-            <BsCalendar4Week /> {zone.runtimePerWeek} days
+            <BsCalendar4Week />
+            {zone.runtimePerWeek} days
           </CardSubheaderItem>
           <CardSubheaderItem>
-            {getSeasonIcon(zone.season)} {zone.season}
+            {getSeasonIcon(zone.season)}
+            {zone.season}
           </CardSubheaderItem>
           <CardSubheaderItem>
-            <PiPlant /> {zone.totalPlants}
+            <PiPlant />
+            {zone.totalPlants}
           </CardSubheaderItem>
         </CardSubheader>
       }
-    action={
-      <>
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-controls={openCardMenu ? "long-menu" : undefined}
-          aria-expanded={openCardMenu ? "true" : undefined}
-          aria-haspopup="true"
-          onClick={handleCardMenuClick}
-        >
-          <HiOutlineDotsHorizontal />
-        </IconButton>
-        <Menu
-          id="long-menu"
-          anchorEl={anchorElCardMenu}
-          open={openCardMenu && Boolean(anchorElCardMenu)}
-          onClose={handleCardMenuClose}
-          slotProps={{
-            paper: {
-              sx: {
-                backgroundColor: zoneCard.card.backgroundColor,
-                color: zoneCard.header.color,
-                boxShadow: 3,
-                borderRadius: 2,
-              },
-            },
-          }}
-          MenuListProps={{
-            "aria-labelledby": "long-button",
-            sx: { padding: 0 },
-          }}
-        >
-          {options.map((option) => (
-            <MenuItem
-              key={option}
-              onClick={handleCardMenuSelect(option)}
-              sx={{
-                color: zoneCard.text.color,
-                ...fonts.content,
-                fontSize: '0.95rem',
-                '&:hover': {
+      action={
+        <>
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls={openCardMenu ? "long-menu" : undefined}
+            aria-expanded={openCardMenu ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleCardMenuClick}
+          >
+            <HiOutlineDotsHorizontal />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            anchorEl={anchorElCardMenu}
+            open={openCardMenu && Boolean(anchorElCardMenu)}
+            onClose={handleCardMenuClose}
+            slotProps={{
+              paper: {
+                sx: {
+                  backgroundColor: zoneCard.card.backgroundColor,
                   color: zoneCard.header.color,
+                  boxShadow: 3,
+                  borderRadius: 2,
                 },
+              },
+            }}
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+              sx: { padding: 0 },
+            }}
+          >
+            {options.map((option) => (
+              <MenuItem
+                key={option}
+                onClick={handleCardMenuSelect(option)}
+                sx={{
+                  color: zoneCard.text.color,
+                  ...fonts.content,
+                  fontSize: "0.95rem",
+                  "&:hover": {
+                    color: zoneCard.header.color,
+                  },
+                }}
+              >
+                {option}
+              </MenuItem>
+            ))}
+          </Menu>
+          <Popover
+            id={id}
+            open={Boolean(anchorEl) && !!anchorEl}
+            anchorEl={anchorEl}
+            onClose={handleDeleteClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            slotProps={{
+              paper: {
+                sx: {
+                  backgroundColor: zoneCard.card.backgroundColor,
+                  color: zoneCard.header.color,
+                  boxShadow: 3,
+                  borderRadius: 2,
+                  minWidth: 220,
+                  p: "1.5rem 1.5rem 1rem 1.5rem",
+                },
+              },
+            }}
+          >
+            <span
+              style={{
+                display: "flex",
+                marginBottom: "1rem",
+                alignItems: "center",
+                ...fonts.content,
               }}
             >
-              {option}
-            </MenuItem>
-          ))}
-        </Menu>
-        <Popover
-          id={id}
-          open={Boolean(anchorEl) && !!anchorEl}
-          anchorEl={anchorEl}
-          onClose={handleDeleteClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          slotProps={{
-            paper: {
-              sx: {
-                backgroundColor: zoneCard.card.backgroundColor,
-                color: zoneCard.header.color,
-                boxShadow: 3,
-                borderRadius: 2,
-                minWidth: 220,
-                p: '1.5rem 1.5rem 1rem 1.5rem',
-              }
-            }
-          }}
-        >
-          <span style={{ 
-            display: 'flex', 
-            marginBottom: '1rem', 
-            alignItems: 'center',
-            ...fonts.content,
-          }}>
-            <PiWarningFill className="message-icon" style={ messages.warning.icon } /> This will delete all associated plants.
+              <PiWarningFill
+                className="message-icon"
+                style={messages.warning.icon}
+              />{" "}
+              This will delete all associated plants.
             </span>
-          <PopoverActions>
-            <StyledButton
-              sx={{
-                backgroundColor: zoneCard.button.backgroundColor,
-                color: zoneCard.button.color,
-                ...fonts.content,
-                '&:hover': {
-                  backgroundColor: zoneCard.button['&:hover'].backgroundColor,
-                  color: zoneCard.button['&:hover'].color,
-                },
-              }}
-              onClick={deleteZone}
-            >
-              Confirm
-            </StyledButton>
-            <StyledButton
-              sx={{
-                backgroundColor: zoneCard.button.backgroundColor,
-                color: zoneCard.button.color,
-                ...fonts.content,
-                '&:hover': {
-                  backgroundColor: zoneCard.button['&:hover'].backgroundColor,
-                  color: zoneCard.button['&:hover'].color,
-                },
-              }}
-              onClick={handleDeleteClose}
-            >
-              Cancel
-            </StyledButton>
-          </PopoverActions>
-        </Popover>
-      </>
-    }
-  />
+            <PopoverActions>
+              <StyledButton
+                sx={{
+                  backgroundColor: zoneCard.button.backgroundColor,
+                  color: zoneCard.button.color,
+                  ...fonts.content,
+                  "&:hover": {
+                    backgroundColor: zoneCard.button["&:hover"].backgroundColor,
+                    color: zoneCard.button["&:hover"].color,
+                  },
+                }}
+                onClick={deleteZone}
+              >
+                Confirm
+              </StyledButton>
+              <StyledButton
+                sx={{
+                  backgroundColor: zoneCard.button.backgroundColor,
+                  color: zoneCard.button.color,
+                  ...fonts.content,
+                  "&:hover": {
+                    backgroundColor: zoneCard.button["&:hover"].backgroundColor,
+                    color: zoneCard.button["&:hover"].color,
+                  },
+                }}
+                onClick={handleDeleteClose}
+              >
+                Cancel
+              </StyledButton>
+            </PopoverActions>
+          </Popover>
+        </>
+      }
+    />
   );
 };
 
@@ -205,7 +224,8 @@ const CardSubheader = styled.div`
   display: flex;
   align-items: center;
   gap: 1.25rem;
-  font-size: 0.75rem;
+  font-size: 0.85rem;
+  white-space: nowrap;
   border-radius: 5px;
   margin: 0.25rem 0px 0rem 0 !important;
 `;
